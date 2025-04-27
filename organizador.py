b10ab72871db6b2e741ed8420bf2e7579ad426fb
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-04-27 17:16:26 -0300

    Versão final do código do desafio de dados

--- organizador.py
+++ organizador.py
@@ -2,13 +2,6 @@ import pandas as pd
 
 baseDesorganizada = pd.read_csv('baseDesorganizada.csv')
 
-mapeamento = {
-    'fem': 'Feminino',
-    'F': 'Feminino',
-    'masc': 'Masculino',
-    'M': 'Masculino'
-}
-
 def tratarMedia(x):
     return str(x).replace('.', ',')
 
@@ -16,6 +9,12 @@ def tratarMedia(x):
 def tratarNotas(x):
     return float(str(x).replace(',', '.'))
 
+mapeamento = {
+    'fem': 'Feminino',
+    'F': 'Feminino',
+    'masc': 'Masculino',
+    'M': 'Masculino'
+}
 
 baseDesorganizada['sexo'] = baseDesorganizada['sexo'].map(lambda x: mapeamento.get(x, x))
 
@@ -29,5 +28,6 @@ baseDesorganizada['nota_matematica'] = baseDesorganizada['nota_matematica'].map(
 baseDesorganizada['nota_portugues'] = baseDesorganizada['nota_portugues'].map(tratarMedia)
 
 baseDesorganizada['aprovado'] = baseDesorganizada['media'].map(lambda x: 'Sim' if x >= 7 else 'Não')
+baseDesorganizada['media'] = (baseDesorganizada['media'].map(tratarMedia))
 
 baseDesorganizada.to_csv('baseTratada.csv', index=False)
